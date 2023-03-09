59639fe1cefaff88a1dbbfe6a1f8c0c531bff7fa
Emerson Soares <84353395+EmersonAD@users.noreply.github.com>
2023-03-09 13:00:00 -0300

    [ADD] - navigation configuration + layouts creations

--- app/src/main/java/com/jikan/pexelsapplication/ui/fragment/adapter/photoadapter/PhotoAdapter.kt
+++ app/src/main/java/com/jikan/pexelsapplication/ui/fragment/adapter/photoadapter/PhotoAdapter.kt
@@ -1,11 +1,30 @@
-package com.jikan.pexelsapplication.ui.fragment.adapter
+package com.jikan.pexelsapplication.ui.fragment.adapter.photoadapter
 
+import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.recyclerview.widget.DiffUtil
 import com.jikan.core.model.PhotoDomain
 
-class PhotoAdapter() : PagingDataAdapter<PhotoDomain, PhotoAdapter.PhotoViewHolder> {
-    class PhotoViewHolder {
+class PhotoAdapter(
+    private val photoCallback: ((photo: PhotoDomain) -> Unit),
+) : PagingDataAdapter<PhotoDomain, PhotoViewHolder>(diffCallback) {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder =
+        PhotoViewHolder.create(parent, photoCallback)
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        getItem(position)?.let { holder.bind(it) }
+    }
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<PhotoDomain>() {
+            override fun areItemsTheSame(oldItem: PhotoDomain, newItem: PhotoDomain): Boolean {
+                return oldItem.url == newItem.url
+            }
+
+            override fun areContentsTheSame(oldItem: PhotoDomain, newItem: PhotoDomain): Boolean {
+                return oldItem == newItem
+            }
+        }
     }
 }
--- app/src/main/java/com/jikan/pexelsapplication/ui/fragment/main/MainFragment.kt
+++ app/src/main/java/com/jikan/pexelsapplication/ui/fragment/main/MainFragment.kt
@@ -5,56 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import com.jikan.pexelsapplication.R
+import com.jikan.pexelsapplication.databinding.FragmentMainBinding
+import com.jikan.pexelsapplication.ui.fragment.category.CategoryFragment
+import com.jikan.pexelsapplication.ui.fragment.collections.CollectionsFragment
+import com.jikan.pexelsapplication.ui.fragment.pageradapter.ViewPagerAdapter
+import com.jikan.pexelsapplication.ui.fragment.popular.PopularFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentMainBinding
+    private val tabTitles = listOf("Popular", "Collections", "Category")
+    private val fragments = listOf(PopularFragment(), CollectionsFragment(), CategoryFragment())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolbar()
+        initViewPager()
+        initTabLayout()
+    }
+
+    private fun initViewPager() {
+        val pageAdapter = ViewPagerAdapter(context as FragmentActivity, fragments)
+        binding.run {
+            viewPager.adapter = pageAdapter
+        }
+    }
+
+    private fun initTabLayout() {
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+    }
+
+    private fun initToolbar() {
+        binding.toolbar.title = getString(R.string.app_name)
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
     }
 }
