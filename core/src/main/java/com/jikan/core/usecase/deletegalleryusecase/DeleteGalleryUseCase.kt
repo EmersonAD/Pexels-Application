e4f8ef2e58fdc52cd904f837dd3360cb26d1180f
Emerson Soares <84353395+EmersonAD@users.noreply.github.com>
2023-03-23 13:00:00 -0300

    [ADD] - Gallery useCases

--- core/src/main/java/com/jikan/core/usecase/deletegalleryusecase/DeleteGalleryUseCase.kt
+++ core/src/main/java/com/jikan/core/usecase/deletegalleryusecase/DeleteGalleryUseCase.kt
@@ -1,4 +1,28 @@
 package com.jikan.core.usecase.deletegalleryusecase
 
+import com.jikan.core.data.dbrepository.GalleryRepository
+import com.jikan.core.model.PhotoDomain
+import com.jikan.core.usecase.base.CoroutineDispatchers
+import com.jikan.core.usecase.base.ResultStatus
+import com.jikan.core.usecase.base.UseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
 interface DeleteGalleryUseCase {
+    operator fun invoke(params: Params): Flow<ResultStatus<Unit>>
+    data class Params(val photoDomain: PhotoDomain)
+}
+
+class DeleteGalleryUseCaseImpl @Inject constructor(
+    private val galleryRepository: GalleryRepository,
+    private val dispatchers: CoroutineDispatchers,
+) : UseCase<DeleteGalleryUseCase.Params, Unit>(), DeleteGalleryUseCase {
+
+    override suspend fun doWork(params: DeleteGalleryUseCase.Params): ResultStatus<Unit> {
+        return withContext(dispatchers.io()) {
+            galleryRepository.delete(params.photoDomain)
+            ResultStatus.Success (Unit)
+        }
+    }
 }
--- core/src/main/java/com/jikan/core/usecase/insertgalleryusecase/InsertGalleryUseCase.kt
+++ core/src/main/java/com/jikan/core/usecase/insertgalleryusecase/InsertGalleryUseCase.kt
@@ -1,4 +1,28 @@
 package com.jikan.core.usecase.insertgalleryusecase
 
+import com.jikan.core.data.dbrepository.GalleryRepository
+import com.jikan.core.model.PhotoDomain
+import com.jikan.core.usecase.base.CoroutineDispatchers
+import com.jikan.core.usecase.base.ResultStatus
+import com.jikan.core.usecase.base.UseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
 interface InsertGalleryUseCase {
+    operator fun invoke(params: Params): Flow<ResultStatus<Unit>>
+    data class Params(val photoDomain: PhotoDomain)
+}
+
+class InsertGalleryUseCaseImpl @Inject constructor(
+    private val repository: GalleryRepository,
+    private val dispatchers: CoroutineDispatchers,
+) : UseCase<InsertGalleryUseCase.Params, Unit>(), InsertGalleryUseCase {
+
+    override suspend fun doWork(params: InsertGalleryUseCase.Params): ResultStatus<Unit> {
+        return withContext(dispatchers.io()) {
+            repository.insert(params.photoDomain)
+            ResultStatus.Success(Unit)
+        }
+    }
 }
