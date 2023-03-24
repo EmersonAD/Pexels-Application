5a238b42e6478d506e5d59d8004c9b1f7e3cdc9b
Emerson Soares <84353395+EmersonAD@users.noreply.github.com>
2023-03-24 13:00:00 -0300

    [ADD] - Gallery adapter + extensions

--- app/src/main/java/com/jikan/pexelsapplication/ui/fragment/adapter/galleryadapter/GalleryAdapter.kt
+++ app/src/main/java/com/jikan/pexelsapplication/ui/fragment/adapter/galleryadapter/GalleryAdapter.kt
@@ -1,4 +1,33 @@
 package com.jikan.pexelsapplication.ui.fragment.adapter.galleryadapter
 
-class GalleryAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.jikan.core.model.PhotoDomain
+import com.jikan.pexelsapplication.ui.fragment.adapter.photoadapter.PhotoViewHolder
+
+class GalleryAdapter(
+    private val clickCallback: ((photo: PhotoDomain) -> Unit),
+    private val longClickCallback: ((photo: PhotoDomain) -> Unit),
+) : ListAdapter<PhotoDomain, PhotoViewHolder>(differCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        return PhotoViewHolder.create(parent, clickCallback, longClickCallback)
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        getItem(position)?.let { holder.bind(it) }
+    }
+
+    companion object {
+        private val differCallback = object : DiffUtil.ItemCallback<PhotoDomain>() {
+            override fun areItemsTheSame(oldItem: PhotoDomain, newItem: PhotoDomain): Boolean {
+                return oldItem.url == newItem.url
+            }
+
+            override fun areContentsTheSame(oldItem: PhotoDomain, newItem: PhotoDomain): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
 }
